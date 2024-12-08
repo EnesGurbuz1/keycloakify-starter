e53d8e73ef40e8b9c985539a4e07bcf4f5871e96
EnesGurbuz1 <enesbugragurbuz1@gmail.com>
2024-12-08 18:00:58 +0300

    Create: login and register page themes and stories

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -4,10 +4,12 @@ import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
 import Template from "keycloakify/login/Template";
+import "./main.css";
 const UserProfileFormFields = lazy(
     () => import("keycloakify/login/UserProfileFormFields")
 );
-
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
 const doMakeUserConfirmPassword = true;
 
 export default function KcPage(props: { kcContext: KcContext }) {
@@ -19,6 +21,23 @@ export default function KcPage(props: { kcContext: KcContext }) {
         <Suspense>
             {(() => {
                 switch (kcContext.pageId) {
+                    case "login.ftl": return (
+                        <Login
+                            {...{ kcContext, i18n, classes }}
+                            {...{ kcContext, i18n, classes }}
+                            Template={Template}
+                            doUseDefaultCss={true}
+                        />
+                    );
+                    case "register.ftl": return (
+                        <Register
+                            {...{ kcContext, i18n, classes }}
+                            Template={Template}
+                            doUseDefaultCss={true}
+                            UserProfileFormFields={UserProfileFormFields}        
+                            doMakeUserConfirmPassword={doMakeUserConfirmPassword}
+                        />
+                    );
                     default:
                         return (
                             <DefaultPage
@@ -37,4 +56,7 @@ export default function KcPage(props: { kcContext: KcContext }) {
     );
 }
 
-const classes = {} satisfies { [key in ClassKey]?: string };
+const classes = {
+    kcBodyClass:"bg-color",
+    kcHtmlClass:"",
+} satisfies { [key in ClassKey]?: string };
